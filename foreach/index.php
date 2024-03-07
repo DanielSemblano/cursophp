c6629bf54f2156b0f6384e030a8a087bfe59527e
Daniel Semblano <d.semblano2@gmail.com>
2024-03-06 21:07:47 -0300

    adicionando foreach a meu arquivo index

---
+++ foreach/index.php
@@ -0,0 +1,25 @@
+<?php
+$clientes = [
+    ["name" => "Peter Parker", "email" => "parker.peter@hotmail.com"],
+    ["name" => "Mery Jane", "email" => "maryjane@yahoo.com"],
+    ["name" => "Gwen Stacy", "email" => "stacy.gwen@gmail.com"],
+    ["name" => "Otto Octavius", "email" => "dr.octavius@outlook.com"],
+    ["name" => "Norman Osborn", "email" => "greengoblin@osborn.com"],
+    ["name" => "Harry Osborn", "email" => "greengoblin2@osborn.com"],
+];
+
+foreach ($clientes as $cliente) {
+    echo "<b>Nome: </b>" . $cliente["name"] . "<br>";
+    echo "<strong>E-mail: </strong>" . $cliente["email"] . "<br>";
+    echo "<br>";
+}
+
+echo "<hr>";
+
+// echo "<pre>";
+// print_r($clientes);
+// echo "</pre>";
+
+foreach ($clientes as $key => $value) {
+    echo  $key . " - " . $value["name"] . " - " . $value["email"] . "<br>";
+}
