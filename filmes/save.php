509c319aaadc33e096e797182d62c4e932818f4e
Daniel Semblano <d.semblano2@gmail.com>
2024-03-13 19:32:28 -0300

    finalizado codigo filmes

--- filmes/save.php
+++ filmes/save.php
@@ -1,4 +1,5 @@
 <?php
+/* RECEBENDO AS VARIAVES DO FORMULÁRIO */
 $filme =isset($_POST["filme"]) ? $_POST["filme"]: "";
 $acao =isset($_POST["acao"]) ? $_POST["acao"]: "";
 $aventura =isset($_POST["aventura"]) ? $_POST["aventura"]: "";
@@ -8,7 +9,7 @@ $infantil =isset($_POST["infantil"]) ? $_POST["infantil"]: "";
 $suspense =isset($_POST["suspense"]) ? $_POST["suspense"]: "";
 $terror =isset($_POST["terror"]) ? $_POST["terror"]: "";
 
-$categotias = [
+$categorias = [
     "acao" => $acao, 
     "aventura" => $aventura, 
     "comedia" => $comedia, 
@@ -18,6 +19,31 @@ $categotias = [
     "terror" => $terror
 ];
 
-echo "<pre>";
-print_r($categorias);
-echo "</pre>";
+// echo "<pre>";
+// print_r($categorias);
+// echo "</pre>";
+?>
+<!-- EXIBINDO O HTML COM AS INFORMAÇÕES ENVIADAS PELO FORMULARIO -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do filme</title>
+</head>
+<body>
+    <p>Filme: <?php echo $filme; ?></p>
+    <p>Categorias: </p>
+    <ul>
+    <?php
+    foreach ($categorias as $key => $value) {
+        if ($value) {
+            echo "<li>" . $key . "</li> <br>";
+        }
+    }
+    ?>
+    </ul>
+</body>
+</html>
+
