4a6aa2ec58c9f335a78c92e238e68412b93df31b
Daniel Semblano <d.semblano2@gmail.com>
2024-03-19 23:30:30 -0300

    Estilos implementados no site

--- calc/index.php
+++ calc/index.php
@@ -3,20 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">  
     <title>Calculadora</title>
 </head>
 <body>
-    <h2>Calculadora</h2>
+    <div class="fundo">
+        <h2>Calculadora</h2>
+    </div>
+    <div class="calculadora">
     <Form method="POST" action="calculadora.php">
     <label><b>Escolha uma opção de calculo:</b></label> <br>  
         <input type="radio" name="calculo" value="adicao"> Adição <br>
         <input type="radio" name="calculo"  value="subtracao"> Subtração <br>
         <input type="radio" name="calculo"  value="multiplicacao"> Multipicação <br>
         <input type="radio" name="calculo"  value="divisao"> Divisão <br>
-        <input type="submit" value="Calcular">
+        <input class="botao" type="submit" value="Calcular">
     </Form>
+    </div>
 </body>
 </html>
+
 
 
 
